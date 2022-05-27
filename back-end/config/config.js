2f13b6a52e3885b94cdf07a384cd8b411b514766
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2022-05-27 12:43:04 -0300

    refactor: changes config.js file

---
+++ back-end/config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: 'web_chat',
+    host: process.env.HOSTNAME,
+    dialect: 'mysql',
+  },
+  test: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: 'web_chat',
+    host: process.env.HOSTNAME,
+    dialect: 'mysql',
+  },
+  production: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: 'web_chat',
+    host: process.env.HOSTNAME,
+    dialect: 'mysql',
+  },
+};
