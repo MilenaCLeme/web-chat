3e9363be1706b7e33bb33d14569b584fc06317f0
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2022-05-27 15:12:13 -0300

    feat: creates login service

--- back-end/services/login/login.js
+++ back-end/services/login/login.js
@@ -1,26 +1,23 @@
-// const { StatusCodes } = require('http-status-codes');
-// const Models = require('../../models');
-// const { nonExistentUser, invalidData } = require('../../utilities/setErrors');
-// const { genToken } = require('../auth/auth');
-// const { validateLogin } = require('./validations');
+const { StatusCodes } = require('http-status-codes');
+const Models = require('../../models');
+const { nonExistentUser, invalidData } = require('../../utilities/setErrors');
+const { validateLogin } = require('./validations');
 
-// module.exports = async (user) => {
-//   const validateError = validateLogin(user).error;
+module.exports = async (user) => {
+  const validateError = validateLogin(user).error;
   
-//   if (validateError) {
-//     return invalidData(StatusCodes.BAD_REQUEST, validateError.message);
-//   }
-//   const findUser = await Models.Users.findOne({ where: { email: user.email } });
+  if (validateError) {
+    return invalidData(StatusCodes.BAD_REQUEST, validateError.message);
+  }
+  const findUser = await Models.Users.findOne({ where: { email: user.email } });
   
-//   const data = findUser ? findUser.dataValues : null;
+  const data = findUser ? findUser.dataValues : null;
   
-//   if (!findUser || user.password !== data.password) {
-//     return nonExistentUser;
-//   }
-//   delete findUser.password;
-//   const userWPassword = findUser;
+  if (!findUser || user.password !== data.password) {
+    return nonExistentUser;
+  }
+  
+  const userWPassword = findUser.email;
 
-//   const token = genToken(userWPassword);
-
-//   return { status: StatusCodes.OK, message: token };
-// };
+  return { status: StatusCodes.OK, message: userWPassword };
+};
