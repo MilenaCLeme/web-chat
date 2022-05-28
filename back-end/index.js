a7e0c3c39153a4e090978095d667e633fe696ed1
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2022-05-28 09:29:19 -0300

    feat: configurando o cors

--- back-end/index.js
+++ back-end/index.js
@@ -6,9 +6,15 @@ require('dotenv').config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
 
 const PORT = process.env.PORT || 3001;
+
+app.use(
+  cors({
+    origin: `http://localhost:${process.env.PORT}` || 'http://localhost:3001',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  }),
+);
 
 app.use(root);
 app.use(error);
