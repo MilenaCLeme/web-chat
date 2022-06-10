88f9d355c9c83c92fb2195204d60ba4ebf0c1e34
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2022-06-10 16:44:52 -0300

    feat: backend - implementando criação de perguntas

--- back-end/controllers/keyword/create.js
+++ back-end/controllers/keyword/create.js
@@ -1,7 +1,7 @@
 const serviceKeyword = require('../../services/keyword');
 
 module.exports = async (req, res, _next) => {
-  const { keyWord } = req.body
-  const result = await serviceKeyword.keyword(keyWord.toLowerCase());
+  const { keyWord, command, instructions } = req.body;
+  const result = await serviceKeyword.createKeyWord(keyWord, command, instructions);
   return res.status(200).json(result);
 };
--- back-end/controllers/keyword/router.js
+++ back-end/controllers/keyword/router.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const rescue = require('express-rescue');
-const keyword = require('./keyword');
+const getKeyWord = require('./get');
+const createKeyWord = require('./create');
 
 const router = express.Router({ mergeParams: true });
 
-router.post('/', rescue(keyword));
+router.post('/', rescue(getKeyWord));
+router.post('/new', rescue(createKeyWord));
 
 module.exports = router;
