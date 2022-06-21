5b6c69714f0e7f536a4499418ab2846996ea0f58
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2022-06-21 12:26:43 -0300

    feat: creates admin root

--- back-end/controllers/root.js
+++ back-end/controllers/root.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const login = require('./userLogin/router');
 const keyword = require('./keyword/router');
+const admin = require('./admin/router');
 
 const root = express.Router({ mergeParams: true });
 
 root.use('/userlogin', login);
 root.use('/keyword', keyword);
+root.use('/admin', keyword);
+
 
 module.exports = root;
