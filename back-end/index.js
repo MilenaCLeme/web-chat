ec67a630c3c6bf5c425554e82cc683d213070069
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2022-06-23 09:57:57 -0300

    feat: configurando backend para o socket.io

--- back-end/index.js
+++ back-end/index.js
@@ -2,15 +2,37 @@ const express = require('express');
 const cors = require('cors');
 const error = require('./middleware/error');
 const root = require('./controllers/root');
+const http = require("http");
+const { Server } = require("socket.io");
+const PORT = process.env.PORT || 3001;
+
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
 
-const PORT = process.env.PORT || 3001;
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
 app.use(cors());
 app.use(root);
 app.use(error);
 
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`); // quando conectar no front vai aparecer o id do user
+  // socket.on("", () => {});
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
 app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
+
+// server.listen(PORT, () => {
+//   console.log(`ouvindo porta ${PORT}!`);
+// });
