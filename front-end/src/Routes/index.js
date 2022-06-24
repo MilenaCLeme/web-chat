a9facac80b208b0c195a3be67f4afc6fc4bef83e
MilenaCLeme <milenaleme4@hotmail.com>
2022-06-24 16:08:57 -0300

    feas: modified router and insert new routes

--- front-end/src/Routes/index.js
+++ front-end/src/Routes/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from '../Pages/Home';
+import Seller from '../Pages/Seller';
 
 export default function Router() {
   return (
     <Routes>
       <Route exact path="/" element={<Home />} />
+      <Route exact path="/seller" element={<Seller />} />
     </Routes>
   );
 }
