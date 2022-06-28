387402be6185ed786a8a66a1466296abea8e4342
MilenaCLeme <milenaleme4@hotmail.com>
2022-06-28 16:11:12 -0300

    feas: insert props chat

--- front-end/src/Pages/Seller.jsx
+++ front-end/src/Pages/Seller.jsx
@@ -1,43 +1,55 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useContext } from 'react';
+import socket from '../server';
 import Button from '../Components/Button';
 import Chat from '../Components/Chat';
+import MyContext from '../Context';
 
 function Seller() {
-  const [contacts, setContacts] = useState([]);
-  const [message, setMessage] = useState({ name: '', mensagem: [] });
+  const [idPeople, setIdPeople] = useState();
   const [text, setText] = useState('');
 
-  useEffect(() => {
-    setContacts([
-      {
-        name: 'fabio',
-        email: 'fabio2@gmail.com',
-        mensagem: [
-          {
-            type: 'client',
-            text: 'Oi, preciso de ajuda??',
-          },
-        ],
-      },
-    ]);
-  }, []);
+  const { callAll } = useContext(MyContext);
+
+  const message = callAll.filter((tipo) => tipo.id === idPeople)[0];
+
+  const insertMessage = (t) => {
+    const arrayNew = callAll.filter((tipo) => tipo.id === idPeople)[0];
+    const jsonMessage = {
+      type: 'seller',
+      text: t,
+    };
+    const newArrayMessage = [...arrayNew.message, jsonMessage];
+    const newCall = callAll.map((call) => {
+      if (call.id === idPeople) {
+        return {
+          id: call.id,
+          name: call.name,
+          email: call.email,
+          message: newArrayMessage,
+        };
+      }
+      return { ...call };
+    });
+    socket.emit('send_mensage', newCall);
+  };
+
   return (
     <main>
       <aside>
         {
-          contacts.map(({ name }, index) => (
-            <Button name={name} stl="opcoes" bool={false} func={() => setMessage(contacts[index])} />
+          callAll.map(({ name, id }) => (
+            <Button key={id} name={name} stl="opcoes" bool={false} func={() => setIdPeople(id)} />
           ))
         }
       </aside>
       <section>
         {
-          message.name !== '' && (
+          idPeople && (
             <>
-              <Chat name={message.name} message={message.mensagem} />
+              <Chat typePeople="seller" name={message.name} message={message.message} />
               <div>
                 <input type="text" value={text} onChange={({ target }) => setText(target.value)} />
-                <button type="submit">Enviar</button>
+                <button type="submit" onClick={() => { insertMessage(text); setText(''); }}>Enviar</button>
               </div>
             </>
           )
