371d1c6fb880ade62b50037f203446d3d9dfe58e
MilenaCLeme <milenaleme4@hotmail.com>
2022-06-28 16:09:58 -0300

    style: create two styled for chat

---
+++ front-end/src/styles/chatStyle.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const RightChat = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const LeftChat = styled.div`
+  display: flex;
+  justify-content: flex-start;
+`;
+
+export {
+  RightChat,
+  LeftChat,
+};
