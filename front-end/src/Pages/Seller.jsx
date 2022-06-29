74a9dec1b739ec9da22046d71fd628713eeb2911
MilenaCLeme <milenaleme4@hotmail.com>
2022-06-29 17:20:12 -0300

    feas: modified seller insert styled

--- front-end/src/Pages/Seller.jsx
+++ front-end/src/Pages/Seller.jsx
@@ -1,61 +1,85 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import socket from '../server';
 import Button from '../Components/Button';
 import Chat from '../Components/Chat';
 import MyContext from '../Context';
+import {
+  MainSeller,
+  AsideSeller,
+  SectionSeller,
+  InputWithButton,
+  Info,
+} from '../styles/sellerStyle';
+import { InputChat } from '../styles/homeStyle';
+import getRandom from '../common/utils/getRandom';
 
 function Seller() {
+  const navigate = useNavigate();
   const [idPeople, setIdPeople] = useState();
   const [text, setText] = useState('');
 
-  const { callAll } = useContext(MyContext);
+  const { callAll, roleLogin } = useContext(MyContext);
+
+  useEffect(() => {
+    if (!roleLogin) {
+      navigate('/login');
+    }
+  }, []);
 
   const message = callAll.filter((tipo) => tipo.id === idPeople)[0];
 
   const insertMessage = (t) => {
-    const arrayNew = callAll.filter((tipo) => tipo.id === idPeople)[0];
-    const jsonMessage = {
-      type: 'seller',
-      text: t,
-    };
-    const newArrayMessage = [...arrayNew.message, jsonMessage];
-    const newCall = callAll.map((call) => {
-      if (call.id === idPeople) {
-        return {
-          id: call.id,
-          name: call.name,
-          email: call.email,
-          message: newArrayMessage,
-        };
-      }
-      return { ...call };
-    });
-    socket.emit('send_mensage', newCall);
+    if (t.trim()) {
+      const arrayNew = callAll.filter((tipo) => tipo.id === idPeople)[0];
+      const jsonMessage = {
+        messageId: getRandom(),
+        type: 'seller',
+        text: t,
+      };
+      const newArrayMessage = [jsonMessage, ...arrayNew.message];
+      const newCall = callAll.map((call) => {
+        if (call.id === idPeople) {
+          return {
+            id: call.id,
+            name: call.name,
+            email: call.email,
+            message: newArrayMessage,
+          };
+        }
+        return { ...call };
+      });
+      socket.emit('send_mensage', newCall);
+    }
   };
 
   return (
-    <main>
-      <aside>
+    <MainSeller>
+      <AsideSeller>
         {
-          callAll.map(({ name, id }) => (
-            <Button key={id} name={name} stl="opcoes" bool={false} func={() => setIdPeople(id)} />
-          ))
+          callAll.length > 0 ? callAll.map(({ name, id }) => (
+            <Button key={id} name={name} stl="faleconosco" bool={false} func={() => setIdPeople(id)} />
+          )) : (
+            <Info>
+              <p>Não existe cliente no momento</p>
+            </Info>
+          )
         }
-      </aside>
-      <section>
+      </AsideSeller>
+      <SectionSeller>
         {
           idPeople && (
             <>
-              <Chat typePeople="seller" name={message.name} message={message.message} />
-              <div>
-                <input type="text" value={text} onChange={({ target }) => setText(target.value)} />
-                <button type="submit" onClick={() => { insertMessage(text); setText(''); }}>Enviar</button>
-              </div>
+              <Chat typePeople="seller" name={message.name} message={message.message} basis="70%" />
+              <InputWithButton>
+                <InputChat type="text" value={text} onChange={({ target }) => setText(target.value)} />
+                <Button name="Enviar" func={() => { insertMessage(text); setText(''); }} stl="falecom" bool={false} />
+              </InputWithButton>
             </>
           )
         }
-      </section>
-    </main>
+      </SectionSeller>
+    </MainSeller>
   );
 }
 
