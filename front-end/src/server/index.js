b2e7389e7aa10ba5ffca031ab61505b82f4832e1
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2023-05-19 11:28:39 -0300

    fix: frontend lendo as variaveis de ambiente

--- front-end/src/server/index.js
+++ front-end/src/server/index.js
@@ -1,10 +1,8 @@
 import io from 'socket.io-client';
 
-// eslint-disable-next-line import/no-extraneous-dependencies
-require('dotenv').config();
-
 const HOST = process.env.REACT_APP_API_HOST || 'localhost:3001';
 const PROTOCOL = process.env.REACT_APP_API_PROTOCOL || 'http';
+
 const socket = io.connect(`${PROTOCOL}://${HOST}`);
 
 export default socket;
